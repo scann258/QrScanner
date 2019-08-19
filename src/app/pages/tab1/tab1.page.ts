d962dbcb535b7a115088312d7c7556ed89157f88
Alejandro ACER <scann258@hotmail.com>
2019-08-19 14:39:57 -0300

    Primer Commit

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+
+  slideOpts = {
+
+    allowSlidePrev: false,
+    allowSlideNext: false
+
+  };
+
+  constructor( private barcodeScanner: BarcodeScanner) {}
+
+
+  ionViewWillEnter() {
+
+    this.scan();
+
+  }
+
+
+  scan() {
+
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+
+  }
+
+
+
+
+
+}
