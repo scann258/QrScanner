18585efcb10aa0b73bd054c747196d4cc1617279
Alejandro ACER <scann258@hotmail.com>
2019-08-22 18:53:07 -0300

    Creando el tab Historial, Modelo de Registros y dataLocalService

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
@@ -16,7 +17,8 @@ export class Tab1Page {
 
   };
 
-  constructor( private barcodeScanner: BarcodeScanner) {}
+  constructor( private barcodeScanner: BarcodeScanner,
+               private dataLocal: DataLocalService) {}
 
 
   ionViewWillEnter() {
@@ -30,8 +32,16 @@ export class Tab1Page {
 
     this.barcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
+
+      if ( !barcodeData.cancelled ) {
+        this.dataLocal.guardarRegistro( barcodeData.format, barcodeData.text );
+      }
+
+
      }).catch(err => {
          console.log('Error', err);
+
+         this.dataLocal.guardarRegistro( 'QRCode', 'http://www.google.com' );
      });
 
   }
